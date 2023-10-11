e1d2f8653b0128b4f787cc35cf01907b96098d8b
Oksana <oksana.s.kovalchuk@gmail.com>
2023-10-11 18:24:15 +0300

    add todolist stories

---
+++ src/features/TodolistList/ui/Todolist/Todolist.stories.tsx
@@ -0,0 +1,87 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { StoreProviderDecorator } from 'app/model/stories/storeProviderDecorator'
+import { TaskPriorities, TaskStatuses } from 'common/enums'
+import { Todolist } from 'features/TodolistList/ui/Todolist/Todolist'
+
+const meta: Meta<typeof Todolist> = {
+    title: 'TODOLIST/Todolist',
+    component: Todolist,
+    tags: ['autodocs'],
+    decorators: [StoreProviderDecorator],
+}
+
+export default meta
+type Story = StoryObj<typeof Todolist>
+
+export const TodolistStory: Story = {
+    args: {
+        todolist: {
+            id: '1',
+            title: 'What to buy',
+            filter: 'all',
+            entityStatus: 'idle',
+        },
+        tasks: [
+            {
+                id: '1',
+                title: 'Bread',
+                description: '',
+                todoListId: '1',
+                order: 0,
+                status: TaskStatuses.New,
+                priority: TaskPriorities.Middle,
+                startDate: '',
+                deadline: '',
+                addedDate: '',
+            },
+            {
+                id: '2',
+                title: 'Water',
+                description: '',
+                todoListId: '1',
+                order: 0,
+                status: TaskStatuses.New,
+                priority: TaskPriorities.Middle,
+                startDate: '',
+                deadline: '',
+                addedDate: '',
+            },
+            {
+                id: '3',
+                title: 'Cheese',
+                description: '',
+                todoListId: '1',
+                order: 0,
+                status: TaskStatuses.Completed,
+                priority: TaskPriorities.Middle,
+                startDate: '',
+                deadline: '',
+                addedDate: '',
+            },
+            {
+                id: '4',
+                title: 'Juice',
+                description: '',
+                todoListId: '1',
+                order: 0,
+                status: TaskStatuses.New,
+                priority: TaskPriorities.Middle,
+                startDate: '',
+                deadline: '',
+                addedDate: '',
+            },
+            {
+                id: '5',
+                title: 'Fish',
+                description: '',
+                todoListId: '1',
+                order: 0,
+                status: TaskStatuses.Completed,
+                priority: TaskPriorities.Middle,
+                startDate: '',
+                deadline: '',
+                addedDate: '',
+            },
+        ],
+    },
+}
